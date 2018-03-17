787b44035fefaa2973444ea9b7aadfa9ad434242
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2018-03-17 11:31:24 +0530

    Add Recognition in Classifier

--- app/src/main/java/com/amitshekhar/tflite/Classifier.java
+++ app/src/main/java/com/amitshekhar/tflite/Classifier.java
@@ -2,13 +2,71 @@ package com.amitshekhar.tflite;
 
 import android.graphics.Bitmap;
 
+import java.util.List;
+
 /**
  * Created by amitshekhar on 17/03/18.
  */
 
 public interface Classifier {
 
-    String classifyBitmap(Bitmap bitmap);
+    class Recognition {
+        /**
+         * A unique identifier for what has been recognized. Specific to the class, not the instance of
+         * the object.
+         */
+        private final String id;
+
+        /**
+         * Display name for the recognition.
+         */
+        private final String title;
+
+        /**
+         * A sortable score for how good the recognition is relative to others. Higher should be better.
+         */
+        private final Float confidence;
+
+        public Recognition(
+                final String id, final String title, final Float confidence) {
+            this.id = id;
+            this.title = title;
+            this.confidence = confidence;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Float getConfidence() {
+            return confidence;
+        }
+
+        @Override
+        public String toString() {
+            String resultString = "";
+            if (id != null) {
+                resultString += "[" + id + "] ";
+            }
+
+            if (title != null) {
+                resultString += title + " ";
+            }
+
+            if (confidence != null) {
+                resultString += String.format("(%.1f%%) ", confidence * 100.0f);
+            }
+
+            return resultString.trim();
+        }
+    }
+
+
+    List<Recognition> recognizeImage(Bitmap bitmap);
 
     void close();
 }
